9bfa7d8c8472cd36d13badb215e953fe59b1aca0
Israel255 <62959042+Israel255@users.noreply.github.com>
2020-06-16 17:16:49 +0300

    Add files via upload

---
+++ LESSON_13/Computers_Class/Computer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computers_Class
+{
+    [DebuggerDisplay("model: {model} price: {(price/3.67)}")]
+    public class Computer
+    {
+        public string model;
+        public int price;
+        public int numberOfProcessors;
+        public float screenSize;
+        public bool isTurnOn;
+
+        public int tellMeThePrice()
+        {
+            Console.WriteLine("The computer price is: " + price);
+            return price;
+        }
+        public float tellTheScreenSize()
+        {
+            Console.WriteLine("The computer screen size is: " + screenSize);
+            return screenSize;
+        }
+        public void turnOn()
+        {
+            if (isTurnOn == true)
+            {
+                Console.WriteLine("the " + model + " computer is on alredy");
+            }
+            else
+            {
+                Console.WriteLine("Turning on the " + model + " computer");
+                isTurnOn = true;
+            }
+        }
+        public void turnOff()
+        {
+            if (isTurnOn == false)
+            {
+                Console.WriteLine("the " + model + " computer is off alredy");
+            }
+            else
+            {
+                Console.WriteLine("Turning off the " + model + " comuter");
+                isTurnOn = false;
+            }
+        }
+        public void addProcessor()
+        {
+            numberOfProcessors++;
+        }
+        public override string ToString()
+        {
+            return "Computer Model: " + model + " Computer price: " + price + " Number Of Precessors: " + numberOfProcessors + " Screen size: " + screenSize; 
+        }
+    }
+}
---
+++ LESSON_13/Computers_Class/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computers_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer lenovo = new Computer()
+            {
+                model = "yoga",
+                numberOfProcessors = 1,
+                price = 3579,
+                screenSize = 14
+            };
+            lenovo.tellMeThePrice();
+            lenovo.tellTheScreenSize();
+            lenovo.turnOn();
+            lenovo.turnOff();
+            lenovo.addProcessor();
+            Console.WriteLine(lenovo);
+            Console.WriteLine();
+
+            Computer dell = new Computer()
+            {
+                model = "inspiron",
+                numberOfProcessors = 1,
+                price = 3790,
+                screenSize = 15.6f
+            };
+            dell.tellMeThePrice();
+            dell.tellTheScreenSize();
+            dell.turnOn();
+            dell.turnOff();
+            dell.addProcessor();
+            Console.WriteLine(dell);
+            Console.WriteLine();
+
+            Computer apple = new Computer()
+            {
+                model = "macbook pro",
+                numberOfProcessors = 1,
+                price = 17105,
+                screenSize = 16
+            };
+            apple.tellMeThePrice();
+            apple.tellTheScreenSize();
+            apple.turnOn();
+            apple.turnOff();
+            apple.addProcessor();
+            Console.WriteLine(apple);
+            Console.WriteLine();
+
+
+        }
+    }
+}
